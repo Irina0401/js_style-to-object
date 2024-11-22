45b713d7886cb39b694c22ce0749b5997e8898c9
Irina <weedfox053@gmail.com>
2024-11-22 18:07:51 +0200

    Style to object2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,8 +8,7 @@
 function convertToObject(sourceString) {
   const object = {};
 
-  const splitString = sourceString
-    .split(';');
+  const splitString = sourceString.split(';');
 
   splitString.forEach((property) => {
     const [key, ...valueParts] = property.split(':');
@@ -26,70 +25,5 @@ function convertToObject(sourceString) {
 
   return object;
 }
-
-const complexStylesString = `
-  background-color:      #fff;
--webkit-border-radius: 5px;
-  border-radius     : 5px;
-  border: 1px solid #e8e8e8;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  clear   : both       ;
-  cursor: pointer;
-  float: left;
-  font-family: inherit;
-      font-size: 14px;
-  font-weight: 400;
-  height: 42px;
-  line-height:    40px;
-  outline: 0;
-  padding-left    : 18px;
-  padding-right: 30px;
-  ;
-
-  ;
-  position: relative;
-
-  text-align: left !important;
-  -webkit-transition: all .2s ease-in-out;
-  transition: all .2s ease-in-out;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-
-  white-space: nowrap;
-  width: auto;
-`;
-const result = convertToObject(complexStylesString);
-console.log(result === {
-  'background-color': '#fff',
-  '-webkit-border-radius': '5px',
-  'border-radius': '5px',
-  border: '1px solid #e8e8e8',
-  '-webkit-box-sizing': 'border-box',
-  'box-sizing': 'border-box',
-  clear: 'both',
-  cursor: 'pointer',
-  float: 'left',
-  'font-family': 'inherit',
-  'font-size': '14px',
-  'font-weight': '400',
-  height: '42px',
-  'line-height': '40px',
-  outline: '0',
-  'padding-left': '18px',
-  'padding-right': '30px',
-  position: 'relative',
-  'text-align': 'left !important',
-  '-webkit-transition': 'all .2s ease-in-out',
-  transition: 'all .2s ease-in-out',
-  '-moz-user-select': 'none',
-  '-ms-user-select': 'none',
-  'user-select': 'none',
-  'white-space': 'nowrap',
-  width: 'auto',
-}) // outputs true;
-
-console.log(result)
 
 module.exports = convertToObject;
